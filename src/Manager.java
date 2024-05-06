6a8802fba26249be0e64ca1c21e39c4baa26aa05
unknown <MohammadReza Shafeei>
2024-05-06 13:44:34 +0330

    note and manager classes defined

---
+++ src/Manager.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Manager {
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    public ArrayList<Note> getNotes() {
+        return notes;
+    }
+    public void add_note(String topic, String text){}
+    public void remove(int index){}
+    public void Notes_show(int index){}
+    public void export(int index){}
+}
